4cc7c71710ea99b460a78e496c712f80b2416262
syutengu <46765320+syutengu@users.noreply.github.com>
2022-08-30 11:40:05 +0900

    Update calcPi.js

--- calcPi.js
+++ calcPi.js
@@ -1,10 +1,11 @@
-function calcPi(time){
+//estimate Pi using Monte Carlo method
+function calcPi(time) {
     let hit = 0
     for (let i = 0; i < time; i++) {
-        if(Math.random()**2 + Math.random()**2 <= 1) hit++
+        if (Math.random() ** 2 + Math.random() ** 2 <= 1) hit++
     }
-    return hit/time*4
+    return hit / time * 4
 }
 console.time()
-console.log(calcPi(100000000))
+console.log(calcPi(10 ** 8))
 console.timeEnd()
