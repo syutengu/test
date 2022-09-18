e06c68d0da7a02a2804bd370a4baf69d00d808de
syutengu <46765320+syutengu@users.noreply.github.com>
2022-09-18 11:32:20 +0900

    a

--- tengu.js
+++ tengu.js
@@ -23,7 +23,7 @@ function renderElement(parent, type, className, content, options) {
   return el
 }
 
-window.$ = class Dom {
+class Dom {
   /**basic.js定义的Dom对象
    * @param {Dom | HTMLElement} parentDom parent Dom or HTMLElement
    * @param {String} type html element type lowercase
