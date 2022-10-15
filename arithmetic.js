662b751fc1569226cb81c12397706ab1b9401d0d
SYUTENGU <syutengu@gmail.com>
2022-10-16 08:49:11 +0900

    update

--- arithmetic.js
+++ arithmetic.js
@@ -42,9 +42,9 @@ function rounddown(num, digits = 0) {
         return res
 }
 
-//最大安全整数常量
-/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
-Double precision floating point format only has 52 bits to represent the mantissa, 
+//最大安全整数定数
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+/* Double precision floating point format only has 52 bits to represent the mantissa, 
 so it can only safely represent integers between -(2**53 – 1) and 2**53 – 1. 
 "Safe" in this context refers to the ability to represent integers exactly and to compare them correctly. 
 For example, Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2 will evaluate to true, which is mathematically incorrect. */
@@ -52,6 +52,74 @@ console.log(Number.MAX_SAFE_INTEGER)// expected output: 9007199254740991
 console.log(Number.MAX_SAFE_INTEGER + 1)// expected output: 9007199254740992
 console.log(Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2)// expected output: true
 // 100.999999999999999 === 101 //true
+
+
+//モンテカルロ法で円周率の近似値を求める
+//estimate Pi using Monte Carlo method
+function calcPi(time) {
+    let hit = 0
+    for (let i = 0; i < time; i++) {
+        if (Math.random() ** 2 + Math.random() ** 2 <= 1) hit++
+    }
+    return hit / time * 4
+}
+// console.time()
+// console.log(calcPi(10 ** 8))
+// console.timeEnd()
+
+//nの階乗
+function factorialize(n) {
+    return n < 0 ? n : n === 0 ? 1 : n * factorialize(n - 1)
+}
+// console.log(factorialize(16))
+
+
+//素数であるかどうかを判断
+function isPrimeNumber(num) {
+    // 排除非整数与1
+    if (!Number.isInteger(num) || num < 2) return false
+    // if (num > 3 && num % 2 === 0) return false //4以上の偶数はすべて合成数
+    // if (num > 9 && [0, 2, 4, 5, 6, 8].includes(num % 10)) return false　//２桁以上で一の位が0,2,4,5,6,8である整数はすべて合成数
+    // if (num > 9 && num.toString().split('').reduce((a, b) => +a + +b) % 3 === 0) return false //２桁以上3の倍数はすべて合成数
+
+    //除数从2起逐一递增求余
+    // for (let i = 2; i < num; i++) {
+    //     if (num % i === 0) return false
+    // }
+    //优化：如果无法找到小于某数平方根的因数，则该数必为质数
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false
+    }
+
+    return true
+}
+
+
+/**startからendまでの数から素数を見つける
+ * @param {Number} end 終わりの数
+ * @param {Number} start 始まりの数
+ * @return {Array} 見つけた素数の配列
+ */
+ function getPrimeNumber(end, start = 0) {
+    //返す結果の配列
+    let res = []
+    //整数化
+    var start = parseInt(start)
+    var end = parseInt(end)
+    //始まりが整数でなければ、始まりを０にする
+    if (!Number.isInteger(start)) start = 0
+    //終わりが整数でなければ、空の配列を返す
+    if (!Number.isInteger(end)) return res
+    //endよりもstartのほうが大きければスワップ (swap two variables)
+    if (start > end) end = [start, start = end][0]
+    //範囲内でループして見つけた素数を配列に入れる
+    for (let i = start; i < end; i++) {
+        if (isPrimeNumber(i)) res.push(i)
+    }
+    //結果を返す
+    return res
+}
+// console.log(getPrimeNumber(10, 100))
 
 
 /**配列は魔方陣かどうかを判断
@@ -96,68 +164,9 @@ function isMagicSquare(arr) {
 // console.log(t5, isMagicSquare(t5))
 
 
-/**startからendまでの数から素数を見つける
- * @param {Number} end 終わりの数
- * @param {Number} start 始まりの数
- * @return {Array} 見つけた素数の配列
+/* （pending）長さn*nの乱数配列を生成し、魔方陣であるかどうかを判断
+課題：
+１．複数の乱数を範囲指定して生成する　→　当たる確率の高い範囲を考えないと効率悪い
+２．乱数配列が魔方陣に当たる確率はそもそも非常に低い（モンテカルロ法的なやり方は有効な手段？）
  */
-function getPrimeNumber(end, start = 0) {
-    //返す結果の配列
-    let res = []
-    //整数化
-    var start = parseInt(start)
-    var end = parseInt(end)
-    //始まりが整数でなければ、始まりを０にする
-    if (!Number.isInteger(start)) start = 0
-    //終わりが整数でなければ、空の配列を返す
-    if (!Number.isInteger(end)) return res
-    //endよりもstartのほうが大きければスワップ (swap two variables)
-    if (start > end) end = [start, start = end][0]
-    //範囲内でループして見つけた素数を配列に入れる
-    for (let i = start; i < end; i++) {
-        if (isPrimeNumber(i)) res.push(i)
-    }
-    //結果を返す
-    return res
-}
-// console.log(getPrimeNumber(10, 100))
 
-
-//素数であるかどうかを判断
-function isPrimeNumber(num) {
-    // 排除非整数与1
-    if (!Number.isInteger(num) || num < 2) return false
-    // if (num > 3 && num % 2 === 0) return false //4以上の偶数はすべて合成数
-    // if (num > 9 && [0, 2, 4, 5, 6, 8].includes(num % 10)) return false　//２桁以上で一の位が0,2,4,5,6,8である整数はすべて合成数
-    // if (num > 9 && num.toString().split('').reduce((a, b) => +a + +b) % 3 === 0) return false //２桁以上3の倍数はすべて合成数
-
-    //除数从2起逐一递增求余
-    // for (let i = 2; i < num; i++) {
-    //     if (num % i === 0) return false
-    // }
-    //优化：如果无法找到小于某数平方根的因数，则该数必为质数
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return false
-    }
-
-    return true
-}
-
-//モンテカルロ法で円周率の近似値を求める
-//estimate Pi using Monte Carlo method
-function calcPi(time) {
-    let hit = 0
-    for (let i = 0; i < time; i++) {
-        if (Math.random() ** 2 + Math.random() ** 2 <= 1) hit++
-    }
-    return hit / time * 4
-}
-// console.time()
-// console.log(calcPi(10 ** 8))
-// console.timeEnd()
-
-//nの階乗
-function factorialize(n) {
-    return n < 0 ? n : n === 0 ? 1 : n * factorialize(n - 1)
-}
-// console.log(factorialize(16))
